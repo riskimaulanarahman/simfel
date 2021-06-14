9a035050d9e88cfec260000387963713d7892180
riski maulana rahman <riskimaulanarahman@gmail.com>
2021-06-14 23:21:40 +0800

    surat masuk

--- public/assets/js/suratmasuk/suratmasuk.js
+++ public/assets/js/suratmasuk/suratmasuk.js
@@ -132,7 +132,7 @@ $(document).ready(function(){
                 },{ 
                     dataField: "tanggal_terima_surat",
                     width: 150,
-		    sortIndex: 0, sortOrder: "desc",
+		            sortIndex: 0, sortOrder: "asc",
                     dataType:"date", format:"dd-MM-yyyy",displayFormat: "dd-MM-yyyy",
                 },{ 
                     dataField: "asal_surat",
